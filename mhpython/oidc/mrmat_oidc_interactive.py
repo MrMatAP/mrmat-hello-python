20977ac27fa007a0b39f2e7f27d18a14f202225c
Mathieu Imfeld <imfeldma@gmail.com>
2018-11-03 10:43:42 +0800

    Flask authenticates with auth.bobeli.org
    However, it doesn't support dynamic registration yet

--- mhpython/oidc/mrmat_oidc_interactive.py
+++ mhpython/oidc/mrmat_oidc_interactive.py
@@ -6,42 +6,62 @@
 #       to trust its own configured CA when making a request to the
 #       OIDC provider
 
-from flask import Flask
-from flask_oidc import OpenIDConnect
-import ssl
-
-context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-context.verify_mode = ssl.CERT_REQUIRED
-context.load_verify_locations("/Users/imfeldma/var/certs/MrMatCA.pem")
-context.load_cert_chain("/Users/imfeldma/var/certs/nostromo.pem", "/Users/imfeldma/var/certs/nostromo.key")
+import flask
+from flask import Flask, jsonify
+from flask_pyoidc.provider_configuration import ProviderConfiguration, ClientMetadata, ClientRegistrationInfo
+from flask_pyoidc.user_session import UserSession
+from flask_pyoidc.flask_pyoidc import OIDCAuthentication
 
 app = Flask(__name__)
 app.config.update({
+    'SERVER_NAME': 'nostromo.dyn.bobeli.org:5000',
     'SECRET_KEY': 'SomethingReallySecret',
+
     'TESTING': True,
     'DEBUG': True,
-
-    'OIDC_CLIENT_SECRETS': '/Users/imfeldma/var/oidc/mrmat-oidc-interactive.json'
 })
-oidc = OpenIDConnect(app)
+
+client_metadata = ClientMetadata(client_id='mrmat-oidc-interactive-static',
+                                 client_secret='5fa5e476-fe8d-4124-a732-e69cb49c9e64')
+# TODO: This doesn't currently work, causing "ABCMeta object got multiple values for keyword argument 'redirect_uris'
+#client_registration_info = ClientRegistrationInfo(client_name='mrmat-oidc-interactive-dynamic',
+#                                                  client_uri='http://nostromo.dyn.bobeli.org:5000',
+#                                                 contacts=['imfeldma@bobeli.org'])
+config = ProviderConfiguration(issuer='https://auth.bobeli.org/auth/realms/mrmat-test',
+                               #client_registration_info=client_registration_info)
+                               client_metadata=client_metadata)
+auth = OIDCAuthentication({'default': config}, app)
 
 
 @app.route('/')
 def index():
-    if oidc.user_loggedin:
-        return 'Welcome %s' % oidc.user_getfield('email')
-    else:
-        return 'Not logged in'
+    return 'Not logged in'
 
 
 @app.route('/login')
-@oidc.require_login
+@auth.oidc_auth('default')
 def login():
-    return 'Welcome %s' % oidc.user_getfield('email')
+    user_session = UserSession(flask.session)
+    return jsonify(access_token=user_session.access_token,
+                   id_token=user_session.id_token,
+                   userinfo=user_session.userinfo)
+
+
+@app.route('/logout')
+@auth.oidc_logout
+def logout():
+    user_session = UserSession(flask.session)
+    if not user_session.is_authenticated():
+        return "You have been successfully logged out"
+
+
+@auth.error_view
+def error(e=None, error_description=None):
+    return jsonify({'error': e, 'message': error_description})
 
 
 def run():
-    app.run(debug=True, host="0.0.0.0", ssl_context=context)
+    app.run(debug=True, host='0.0.0.0')
 
 
 if __name__ == '__main__':
