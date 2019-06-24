0f1a9f7b952c1524e0c0e98914bafb8070fa508b
Mr Mat <imfeldma@gmail.com>
2019-06-24 08:24:48 +0800

    Localized the spinner

--- mhpython/py_spinner/mrmat_spinner.py
+++ mhpython/py_spinner/mrmat_spinner.py
@@ -5,12 +5,26 @@ import sys
 import time
 import itertools
 import threading
+import gettext
+
+from mhpython.locale import localedir
+
+# Establish the fallback in English
+en = gettext.translation('messages',
+                         localedir=localedir,
+                         languages=['en'])
+# Localize text, fallback to English if we don't have a localization
+t = gettext.translation('messages',
+                        localedir=localedir,
+                        codeset='UTF-8',
+                        fallback=en)
+_ = t.gettext
 
 
 def spinner():
     spin = itertools.cycle("⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏")
     while True:
-        sys.stdout.write(next(spin) + " - Creating host")
+        sys.stdout.write(next(spin) + ' - {}'.format(_("Creating host")))
         sys.stdout.flush()
         time.sleep(0.1)
         sys.stdout.write('\r')
@@ -18,23 +32,23 @@ def spinner():
 
 def lengthy_task():
     time.sleep(5)
-    print("\nDONE")
+    print("\n{}".format(_('Done')))
 
 
 def run():
-    parser = argparse.ArgumentParser(description='Argument Parsing')
-    parser.add_argument('verbose', action='store_true', help='Be verbose')
-    subparsers = parser.add_subparsers(help='sub-command help')
+    parser = argparse.ArgumentParser(description=_('Argument Parser'))
+    parser.add_argument('verbose', action='store_true', help=_('Be verbose'))
+    subparsers = parser.add_subparsers(help=_('sub-command help'))
 
     # Create the parser for the create_alias command
-    parser_create_alias = subparsers.add_parser('create-alias', help='Create an alias')
-    parser_create_alias.add_argument('--fqdn', required=True, help='The FQDN to add the alias to')
-    parser_create_alias.add_argument('--alias', required=True, help='The alias to create')
+    parser_create_alias = subparsers.add_parser('create-alias', help=_('Create an alias'))
+    parser_create_alias.add_argument('--fqdn', required=True, help=_('The FQDN to add the alias to'))
+    parser_create_alias.add_argument('--alias', required=True, help=_('The alias to create'))
 
     # Create the parsers for the create_host command
-    parser_create_host = subparsers.add_parser('create-host', help='Create a host')
-    parser_create_host.add_argument('--fqdn', required=True, help='FQDN to create')
-    parser_create_host.add_argument('--ip', required=True, help='IP to create')
+    parser_create_host = subparsers.add_parser('create-host', help=_('Create a host'))
+    parser_create_host.add_argument('--fqdn', required=True, help=_('FQDN to create'))
+    parser_create_host.add_argument('--ip', required=True, help=_('IP to create'))
 
     args = parser.parse_args()
 
